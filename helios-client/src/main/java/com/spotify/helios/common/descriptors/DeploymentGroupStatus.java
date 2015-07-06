806da9b6656c92adb3db8c872b2cef877e35db4f
David Xia <dxia@spotify.com>
2015-07-06 18:47:44 -0400

    WIP Deployment group status

--- helios-client/src/main/java/com/spotify/helios/common/descriptors/DeploymentGroupStatus.java
+++ helios-client/src/main/java/com/spotify/helios/common/descriptors/DeploymentGroupStatus.java
@@ -21,11 +21,14 @@
 
 package com.spotify.helios.common.descriptors;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Objects;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.Collections.EMPTY_LIST;
@@ -43,33 +46,50 @@ public class DeploymentGroupStatus extends Descriptor {
     DONE,
   }
 
+  public enum DisplayState {
+    ACTIVE,
+    FAILED,
+  }
+
   private final DeploymentGroup deploymentGroup;
   private final State state;
+  private final DisplayState displayState;
   private final List<RolloutTask> rolloutTasks;
+  private final List<String> hosts;
   private final int taskIndex;
+  private final int hostIndex;
   private final String error;
   private final int version;
 
   public static DeploymentGroupStatus of(final DeploymentGroup deploymentGroup,
                                          final State state) {
-    return new DeploymentGroupStatus(deploymentGroup, state, EMPTY_LIST, 0, null, 0);
+    return new DeploymentGroupStatus(deploymentGroup, state, displayState(state), EMPTY_LIST,
+                                     hosts(EMPTY_LIST), 0, 0, null, 0);
   }
 
   public static DeploymentGroupStatus of(final DeploymentGroup deploymentGroup,
                                          final State state, final List<RolloutTask> rolloutTasks) {
-    return new DeploymentGroupStatus(deploymentGroup, state, rolloutTasks, 0, null, 0);
+    return new DeploymentGroupStatus(deploymentGroup, state, displayState(state), rolloutTasks,
+                                     hosts(rolloutTasks), 0, 0, null, 0);
   }
 
   private DeploymentGroupStatus(
       @JsonProperty("deploymentGroup") final DeploymentGroup deploymentGroup,
       @JsonProperty("state") final State state,
+      @JsonProperty("displayState") final DisplayState displayState,
       @JsonProperty("rolloutTasks") final List<RolloutTask> rolloutTasks,
-      @JsonProperty("taskIndex") final int taskIndex, @JsonProperty("error") final String error,
+      @JsonProperty("hosts") final List<String> hosts,
+      @JsonProperty("taskIndex") final int taskIndex,
+      @JsonProperty("taskIndex") final int hostIndex,
+      @JsonProperty("error") final String error,
       @JsonProperty("version") final int version) {
     this.deploymentGroup = checkNotNull(deploymentGroup, "deploymentGroup");
     this.state = checkNotNull(state, "state");
+    this.displayState = displayState;
     this.rolloutTasks = checkNotNull(rolloutTasks, "rolloutTasks");
+    this.hosts = hosts;
     this.taskIndex = taskIndex;
+    this.hostIndex = hostIndex;
     this.version = version;
     this.error = error;
   }
@@ -87,10 +107,25 @@ public class DeploymentGroupStatus extends Descriptor {
   private DeploymentGroupStatus(final Builder builder) {
     this.deploymentGroup = checkNotNull(builder.deploymentGroup, "deploymentGroup");
     this.state = checkNotNull(builder.state, "state");
+    this.displayState = displayState(state);
     this.rolloutTasks = checkNotNull(builder.rolloutTasks, "rolloutTasks");
+    this.hosts = hosts(rolloutTasks);
     this.taskIndex = checkNotNull(builder.taskIndex, "taskIndex");
+    this.hostIndex = taskIndex / 3;
     this.error = builder.error;
     this.version = checkNotNull(builder.version, "version");
+  }
+
+  private static DisplayState displayState(final DeploymentGroupStatus.State state) {
+    return (state.equals(State.FAILED)) ? DisplayState.FAILED : DisplayState.ACTIVE;
+  }
+
+  private static List<String> hosts(final List<RolloutTask> rolloutTasks) {
+    final Set<String> uniqueHosts = Sets.newLinkedHashSet();
+    for (RolloutTask task : rolloutTasks) {
+      uniqueHosts.add(task.getTarget());
+    }
+    return Lists.newArrayList(uniqueHosts);
   }
 
   public DeploymentGroup getDeploymentGroup() {
@@ -101,12 +136,24 @@ public class DeploymentGroupStatus extends Descriptor {
     return state;
   }
 
+  public DisplayState getDisplayState() {
+    return displayState;
+  }
+
   public List<RolloutTask> getRolloutTasks() {
     return rolloutTasks;
   }
 
+  public List<String> getHosts() {
+    return hosts;
+  }
+
   public int getTaskIndex() {
     return taskIndex;
+  }
+
+  public int getHostIndex() {
+    return hostIndex;
   }
 
   public String getError() {
@@ -145,8 +192,14 @@ public class DeploymentGroupStatus extends Descriptor {
     if (state != that.state) {
       return false;
     }
+    if (displayState != that.displayState) {
+      return false;
+    }
     if (rolloutTasks != null ? !rolloutTasks.equals(that.rolloutTasks)
                              : that.rolloutTasks != null) {
+      return false;
+    }
+    if (hosts != null ? !hosts.equals(that.hosts) : that.hosts != null) {
       return false;
     }
     return !(error != null ? !error.equals(that.error) : that.error != null);
@@ -157,23 +210,13 @@ public class DeploymentGroupStatus extends Descriptor {
   public int hashCode() {
     int result = deploymentGroup != null ? deploymentGroup.hashCode() : 0;
     result = 31 * result + (state != null ? state.hashCode() : 0);
+    result = 31 * result + (displayState != null ? displayState.hashCode() : 0);
     result = 31 * result + (rolloutTasks != null ? rolloutTasks.hashCode() : 0);
+    result = 31 * result + (hosts != null ? hosts.hashCode() : 0);
     result = 31 * result + taskIndex;
     result = 31 * result + (error != null ? error.hashCode() : 0);
     result = 31 * result + version;
     return result;
-  }
-
-  @Override
-  public String toString() {
-    return Objects.toStringHelper(this)
-        .add("deploymentGroup", deploymentGroup)
-        .add("state", state)
-        .add("rolloutTasks", rolloutTasks)
-        .add("taskIndex", taskIndex)
-        .add("error", error)
-        .add("version", version)
-        .toString();
   }
 
   public static class Builder {
